b2a2dac5edeb43c1640658c0145bc2d799254017
JacobCGade <jacobcgade@gmail.com>
2025-04-26 23:36:47 +0200

    chore: remove remains of eslint_d as formatter

--- lua/gade/plugins/formatting.lua
+++ lua/gade/plugins/formatting.lua
@@ -5,21 +5,6 @@ return {
 		local conform = require("conform")
 
 		conform.setup({
-			formatters = {
-				eslint_d = {
-					-- Make sure eslint_d is used as a linter, not just a formatter
-					command = vim.fn.expand("~/.local/share/nvim/mason/bin/eslint_d"),
-					args = { "--fix-to-stdout", "--stdin", "--stdin-filename", "$FILENAME" },
-					cwd = require("conform.util").root_file({
-						".eslintrc",
-						".eslintrc.js",
-						".eslintrc.cjs",
-						".eslintrc.json",
-						"eslint.config.js",
-						"eslint.config.mjs", -- Add this for flat config support
-					}),
-				},
-			},
 			formatters_by_ft = {
 				javascript = { "prettier" },
 				typescript = { "prettier" },
